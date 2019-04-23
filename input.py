f3cd43ab264ca99d0aa00c3145a5cbaef35623ac
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-23 22:05:22 +0200

    Update input.py

--- input.py
+++ input.py
@@ -1,14 +1,61 @@
-from database import *
-from traitement_nombre import *
+from database import AFFICHAGE, LISTE_AFFICHAGE
+
 
 
 class entree:
     def entree(self):
-        Oinput = input('affiche 5 + 5')
 
+        #affiche bonjour
+        #affiche la table de 5
+        #affiche 5 + 5
+        Oinput = input('affiche bonjour')
+        return Oinput
 
+    def split(self, Oinput):
         splitage = Oinput.split()
+        return splitage
+
+
+    def recherche_mot_clé(self, para):
+
+        liste = [i for i in LISTE_AFFICHAGE for j in para if i == j]
+        if liste:
+            dico = [valeur for cle, valeur in AFFICHAGE.items()]
+            return dico, para[1:]
+
+    def nombre_para(self):
+        return 1
+ 
+
+    def fonction(self):
+        corps_debut = 'def function('
+        corps_fin = '):'
+
+        return corps_debut, corps_fin
         
+
+    def écriture_fonction(self, para):
+        with open('écriture.py', 'a') as file:
+            file.write(str(para))
+
+
+    def écriture(self, para):
+        with open('écriture.py', 'a') as file:
+            file.write(str(para))
+
+            
+    def écriture_ligne2(self, para):
+        with open('écriture.py', 'a') as file:
+            file.write('\n')
+            file.write('    ')
+            file.write(para)
+
+
+    def traitement(self, para):
+        splitage = para.split()
+
+
+        print(splitage)
         sortie = []
         
 
@@ -19,8 +66,7 @@ class entree:
                 i = int(i)
                 if i == int(i):
                     print("oui", indexage)
-                    sortie.append([i, indexage])
-                    
+                    sortie.append([i, indexage])     
             except:
                 pass
             
@@ -36,7 +82,28 @@ class entree:
 
 
 
-if __name__ == "__main__":
 
-    entree = entree()
-    entree.entree()
+    def parametre(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
