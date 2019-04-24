21d77dcf7b2dc0b741af115e0b3f5bc6f7c2634e
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-24 22:08:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,81 +1,59 @@
 from input import entree
 import écriture
 
+
+
+
+
+#définir la fonction
+#définir si y'a parametres
+#faut il faire une boucle ?
+#faut il afficher?
+#que faut il afficher ? et ou?
+
+
+
+
+
+#combien de fonction ?
+#quoi return ?
+
+
+
+
+
 class main:
-    def entrée(self):
+    def main(self):
         entrance = entree.entree(self)
-        return entrance
-    
-    def split(self, entrance):
-        return entree.split(self, entrance)
+        split = entree.split(self, entrance)
+        print(split)
 
-    def traitement_mot_clé(self, para):
-        mot_cle = entree.recherche_mot_clé(self, para)
-        return mot_cle
-    
-    def parametre(self):
-        nb_para = entree.nombre_para(self)
-        return nb_para
-
-    def corps_fonction(self):
-        corps = entree.fonction(self)
-        return corps
-
-    def écriture_fonction(self, para):
-        entree.écriture_fonction(self, para)
-
-    def écriture_l2(self, para):
-        entree.écriture_ligne2(self, para)
-
-    def écriture(self, para):
-        entree.écriture(self, para)
-
-
-    def parametrage(self, parametre):
-
-        parametres = ''
-        nom = 'para'
-        compteur = 0
-        for i in range(parametre):
-            name = nom + str(compteur)
-            
-            parametres += name
-            
-            compteur += 1
-            
-        return parametres
         
-    def reload(self, para):
-        entree.reload(self, para)
-    
+        #noms de parametres
+        c = main.parametre()
+
+        a = main.corps_fonction()
+        b = main.traitement_mot_clé(split)
+        parametres = main.parametrage(c, b[2])
+
+        main.écriture_fonction(a[0])
+        main.écriture(parametres)
+        main.écriture_fonction(a[1])
+
+        main.écriture_l2(b[0][0])
+        main.écriture(parametres)
+        main.écriture(b[1])
+        main.écriture(b[0][1])
+
+        main.reload(écriture)
+        écriture.the_function() 
+
 if __name__ == "__main__":
 
 
     main = main()
-    
-    entrance = main.entrée()
-    split = main.split(entrance)
+    main.main()
 
-    #noms de parametres
-    c = main.parametre()
-    parametres = main.parametrage(c)
-
-
-    a = main.corps_fonction()
-    b = main.traitement_mot_clé(split)
-
-
-    main.écriture_fonction(a[0])
-    #main.écriture(parametres)
-    main.écriture_fonction(a[1])
-
-    main.écriture_l2(b[0][0])
-    #main.écriture(parametres)
-    main.écriture(b[1])
-    main.écriture(b[0][1])
-
-    main.reload(écriture)
-    écriture.the_function()
     
 
 
