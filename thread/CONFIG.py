aceb94aaee8d4c74d283680bbc5263b1ec2fa176
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-21 22:42:22 +0200

    Update CONFIG.py

--- thread/CONFIG.py
+++ thread/CONFIG.py
@@ -1,1 +1,15 @@
+
+
 PONCTUATION = ["?",".",",","'",'"',":","!"]
+PONCTUATION_ASSOCIATION = {'?':'question'}
+PONCTUATION_OUT = {'question':'réponse'}
+
+
+PATH_VERBE = 'https://leconjugueur.lefigaro.fr/conjugaison/verbe/{}.html'
+
+PRONOM_PERSONNEL = ['Première personne du singulier',
+                    'Deuxième personne du singulier',
+                    'Troisième personne du singulier',
+                    'Première personne du pluriel',
+                    'Deuxième personne du pluriel',
+                    'Troisième personne du pluriel']
